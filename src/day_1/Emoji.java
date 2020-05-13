46f5810eb5d49fc1e43b4a2bbc0c4cf803f1bb41
tr009068 <59310312+yldzali@users.noreply.github.com>
2020-05-13 17:08:31 +0100

    add Emoji.java

---
+++ src/day_1/Emoji.java
@@ -0,0 +1,14 @@
+package day_1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Emoji {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("User can send emoji");
+
+
+    }
+}
