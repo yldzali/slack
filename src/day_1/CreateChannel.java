60e4366dce5f4e26a3fc3b49689fc8ee612ed143
aliyldz <yldzali@gmail.com>
2020-07-03 12:30:31 +0100

    a

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -6,14 +6,19 @@ public class CreateChannel {
         System.out.println("User is able to create channels");
 
         
-   //  String s= "c";
-   //if(s.contains("c")){
+     String s= "c";
+   if(s.contains("c")){
     System.out.println("contains");
 }else{
     System.out.println("not");
-}   
+}
 
-
+        String s= "c";
+        if(s.contains("c")){
+            System.out.println("contains");
+        }else{
+            System.out.println("not");
+        }
 
     }
 }
